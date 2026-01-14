2dd7117746a1617a72e6700fa45e8320bc6a86cf
Sagesa <152689033+saagesa@users.noreply.github.com>
2026-01-14 21:20:34 +0100

    Refactor Perdoruesi to use Builder pattern

--- src/main/java/model/Perdoruesi.java
+++ src/main/java/model/Perdoruesi.java
@@ -8,13 +8,13 @@ public class Perdoruesi {
     private String fjalekalimi;
     private String telefoni;
 
-    public Perdoruesi(int perdoruesId, String emri, String mbiemri, String emaili, String fjalekalimi, String telefoni) {
-        this.perdoruesId = perdoruesId;
-        this.emri = emri;
-        this.mbiemri = mbiemri;
-        this.emaili = emaili;
-        this.fjalekalimi = fjalekalimi;
-        this.telefoni = telefoni;
+    private Perdoruesi(Builder builder) {
+        this.perdoruesId = builder.perdoruesId;
+        this.emri = builder.emri;
+        this.mbiemri = builder.mbiemri;
+        this.emaili = builder.emaili;
+        this.fjalekalimi = builder.fjalekalimi;
+        this.telefoni = builder.telefoni;
     }
 
     public int getPerdoruesId() { return perdoruesId; }
@@ -28,5 +28,43 @@ public class Perdoruesi {
     public String toString() {
         return emri + " " + mbiemri + " | Email: " + emaili + " | Tel: " + telefoni;
     }
-}
 
+    // -------- Builder --------
+    public static class Builder {
+        private int perdoruesId;
+        private String emri;
+        private String mbiemri;
+        private String emaili;
+        private String fjalekalimi;
+        private String telefoni;
+
+        public Builder setPerdoruesId(int perdoruesId) {
+            this.perdoruesId = perdoruesId;
+            return this;
+        }
+        public Builder setEmri(String emri) {
+            this.emri = emri;
+            return this;
+        }
+        public Builder setMbiemri(String mbiemri) {
+            this.mbiemri = mbiemri;
+            return this;
+        }
+        public Builder setEmaili(String emaili) {
+            this.emaili = emaili;
+            return this;
+        }
+        public Builder setFjalekalimi(String fjalekalimi) {
+            this.fjalekalimi = fjalekalimi;
+            return this;
+        }
+        public Builder setTelefoni(String telefoni) {
+            this.telefoni = telefoni;
+            return this;
+        }
+
+        public Perdoruesi build() {
+            return new Perdoruesi(this);
+        }
+    }
+}
